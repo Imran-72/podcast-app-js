38116a07c881cfba24e14db53a4a11e9df4284fb
Imran Abdurakhmanov <petrow318@gmail.com>
2021-12-10 16:10:18 +0300

    прикрепления слушателя события к form и использование e.preventDefault()

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
 import "./styles.css";
 
-console.log("My app");
+const form = document.getElementById("form");
+const input = form.querySelector("#question-input");
+const submitBtn = input.querySelector("#submit");
+
+form.addEventListener("submit", submitFormHandler);
+
+function submitFormHandler(e) {
+  e.preventDefault();
+  console.log(input.value);
+}
