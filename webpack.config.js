d30fa1f3fc154dd641905fcf2453e2c81a260a24
Imran Abdurakhmanov <petrow318@gmail.com>
2021-12-10 12:57:57 +0300

    установка и настройка Webpack

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const HTMLplugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    filename: "bundle.[chunkhash].js",
+    path: path.resolve(__dirname, "public"),
+  },
+  devServer: {
+    port: 3000,
+  },
+  plugins: [
+    new HTMLplugin({
+      template: "./src/index.html",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+};
