84a52ec19ec82e0944617f44f0de75e193789c5b
Imran Abdurakhmanov <petrow318@gmail.com>
2021-12-10 13:12:40 +0300

    установка style-loader / css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -16,4 +16,12 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
