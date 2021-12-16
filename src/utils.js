3b01d627f0874cbc771afb51412be00c2fb02a90
Imran-72 <petrow318@gmail.com>
2021-12-16 18:16:19 +0300

    создал функцию для отображения модального окна

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,15 @@
 export function isValid(values) {
   return values.length >= 10;
 }
+
+export function createModal(title, content) {
+  const modal = document.createElement("div");
+  modal.classList.add("modal");
+
+  modal.innerHTML = `
+      <h1>${title}</h1>
+      <div class="modal-content">${content}</div>
+    `;
+
+  mui.overlay("on", modal);
+}
